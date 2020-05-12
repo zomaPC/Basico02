0b322f9300159fbc62167a81b59a8effc70aa3cf
Zoma <exelsaga@live.com>
2020-05-12 10:43:24 -0500

    Ejercicio Basico Condisiones "VAcaciones a empleados"

--- Basicas02/src/basicas02/Basicas02.java
+++ Basicas02/src/basicas02/Basicas02.java
@@ -5,17 +5,60 @@
  */
 package basicas02;
 
+import java.util.Scanner;
+
 /**
  *
  * @author exels
  */
 public class Basicas02 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+   public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        String nombre = "";
+        int clave = 0, antiguedad = 0;
+        System.out.println("********************************");
+        System.out.println("* Sistema Vacacional coca-cola*");
+        System.out.println("********************************");
+        
+        System.out.println("¿Cual es el Nombre del trabajador?");
+        nombre = entrada.nextLine();
+        System.out.println("");
+        System.out.println("Escribe la antiguedad");
+        antiguedad = entrada.nextInt();
+        System.out.println("");
+        System.out.println("Clave del trabjador");
+        clave  = entrada.nextInt();
+        System.out.println("");
+        
+        if(clave == 1){
+            if(antiguedad == 1){
+                System.out.println(nombre + "Tiene 6 dias vacaciones");
+            }else if(antiguedad >= 2 && antiguedad <=6){
+                System.out.println(nombre + "Tiene derecho a 14 dias");
+            }else if (antiguedad >= 7 ){
+                System.out.println( nombre + "Tiene derecho a 20 dias");
+            }
+        }else if(clave == 2){
+           if(antiguedad == 1){
+                System.out.println(nombre + "Tiene 7 dias vacaciones");
+            }else if(antiguedad >= 2 && antiguedad <=6){
+                System.out.println(nombre + "Tiene derecho a 15 dias");
+            }else if (antiguedad >= 7 ){
+                System.out.println( nombre + "Tiene derecho a 22 dias");
+            }
+        }else if(clave==3){
+            if(antiguedad == 1){
+                System.out.println(nombre + "Tiene 8 dias vacaciones");
+            }else if(antiguedad >= 2 && antiguedad <=6){
+                System.out.println(nombre + "Tiene derecho a 20 dias");
+            }else if (antiguedad >= 7 ){
+                System.out.println( nombre + "Tiene derecho a 30 dias");
+            }
+        
+        }else{
+            System.out.println("Error, la clave no  existe");}
     }
     
 }
